41dcbf7e2361950c909478e58f2fef36096c391f
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-12 09:45:01 +0530

    added size method

---
+++ size.ts
@@ -0,0 +1,29 @@
+/**
+ * Implement a function size(collection) that takes a collection (an array, object, string, Map, Set)
+ * and returns its size, which represents the number of elements in the collection.
+ */
+/**
+ * Gets the size of `collection` by returning its length for array-like values or the number of own enumerable string keyed properties for objects.
+ *
+ * @param {Array | Object | Map | Set | string | null | undefined} collection The collection to inspect.
+ * @returns {number} Returns the collection size.
+ */
+export default function size(collection) {
+  if (collection == null) {
+    return 0;
+  }
+
+  if (Array.isArray(collection) || typeof collection === 'string') {
+    return collection.length;
+  }
+
+  if (collection instanceof Map || collection instanceof Set) {
+    return collection.size;
+  }
+
+  if (typeof collection === 'object') {
+    return Object.keys(collection).length;
+  }
+
+  return 0;
+}
