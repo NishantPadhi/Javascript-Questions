e9b9f135fdf114c1ff6260e8e2ade2b775b183e0
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-13 19:37:19 +0530

    added function currying with call

--- curry.ts
+++ curry.ts
@@ -22,3 +22,22 @@ export default function curry(func) {
         : curried.apply(this, [...args, arg]);
   };
 }
+
+//Alternative
+/**
+ * @param {Function} func
+ * @return {Function}
+ */
+export default function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.call(this, ...args);
+    }
+
+    return (arg) =>
+      arg === undefined
+        ? curried.call(this, ...args)
+        : curried.call(this, ...args, arg);
+  };
+}
+
