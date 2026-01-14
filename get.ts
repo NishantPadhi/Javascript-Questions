3b98fd1a7f4534833f46b7ac1ab04665897a1ef9
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-14 16:35:57 +0530

    added polyfill for lodash get

---
+++ get.ts
@@ -0,0 +1,33 @@
+/**
+ * Before the optional chaining operator (?.) existed, 
+ * it was sometimes troublesome to access deeply-nested properties in huge JavaScript 
+ * objects when some of the intermediate properties might not be present.
+ */
+/**
+ * @param {Object} objectParam
+ * @param {string|Array<string>} pathParam
+ * @param {*} [defaultValue]
+ * @return {*}
+ */
+export default function get(objectParam, pathParam, defaultValue) {
+  // If the path is a `.` seperated string, use split to convert it to an array.
+  const path = Array.isArray(pathParam) ? pathParam : pathParam.split('.');
+
+  let index = 0;
+  let length = path.length;
+  let object = objectParam;
+
+  // Traverse path in the object, stopping early if a value is null/undefined.
+  while (object != null && index < length) {
+    // We use != null instead of !== null to handle undefined objects too
+    // Access next level in the object using string key (handles numeric indices too).
+    object = object[String(path[index])];
+    index++;
+  }
+
+  // Check if the entire path was successfully traversed. If not, the path is invalid.
+  const value = index && index === length ? object : undefined;
+
+  // Return the found value, or the default if the value resolved to undefined.
+  return value !== undefined ? value : defaultValue;
+}
