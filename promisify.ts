1e819374c1d517d264a1f737d8b8a544dd5e6570
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-15 18:47:08 +0530

    added promisify code

---
+++ promisify.ts
@@ -0,0 +1,21 @@
+/**
+ * Before promises/async/await became the standard, 
+ * it was a convention for async APIs in JavaScript to accept callbacks as the last argument. 
+ * Many async versions of Node.js APIs (e.g. fs.readFile and fs.rm) have such signatures. 
+ * Node.js' util.promisify function was created to wrap around callback-based functions by returning Promises 
+ * so that they can be used with async/await.
+ * Implement a function promisify that takes a function following the common callback-last error-first style, i.e. taking a (err, value) => ... callback as the last argument, and returns a version that returns promises.
+ */
+/**
+ * @callback func
+ * @returns Function
+ */
+export default function promisify(func) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      func.call(this, ...args, (err, result) =>
+        err ? reject(err) : resolve(result),
+      );
+    });
+  };
+}
