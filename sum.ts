8a193c3da0c1f67520cdda1b0d4195090c5fec3e
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-16 17:10:38 +0530

    added sum function

---
+++ sum.ts
@@ -0,0 +1,13 @@
+/**
+ * Implement a sum function that accepts a number and allows for repeated calling with more numbers. 
+ * Calling the function without an argument will sum up all the arguments thus far and return the total.
+ */
+/**
+ * @param {number} valueA
+ * @return {Function}
+ */
+export default function sum(valueA) {
+  return function (valueB) {
+    return valueB === undefined ? valueA : sum(valueA + valueB);
+  };
+}
