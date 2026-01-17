3b9c60d2eff3f007233e4bbe3edce7d37e99f1f2
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-17 17:08:37 +0530

    added chunk method

---
+++ chunk.ts
@@ -0,0 +1,29 @@
+/**
+ * Implement a function chunk(array, [size=1]) that splits the input array into groups of 
+ * length size and returns them within a new array. If array can't be split evenly, 
+ * the final chunk will be the remaining elements. The function should not modify the original input array.
+ */
+/**
+ * @template T
+ * @param {Array<T>} array The array to process.
+ * @param {number} [size=1] The length of each chunk.
+ * @returns {Array<Array<T>>} The new array of chunks.
+ */
+export default function chunk(array, size = 1) {
+  if (!Array.isArray(array) || size < 1) {
+    return [];
+  }
+
+  const result = [];
+  let chunk = [];
+
+  for (let i = 0; i < array.length; i++) {
+    chunk.push(array[i]);
+    if (chunk.length === size || i === array.length - 1) {
+      result.push(chunk);
+      chunk = [];
+    }
+  }
+
+  return result;
+}
