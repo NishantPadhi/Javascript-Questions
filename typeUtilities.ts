97f6581b447a05468889949143906dc036e61cec
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-22 20:30:49 +0530

    added solution for type utilities

--- typeUtilities.ts
+++ typeUtilities.ts
@@ -15,3 +15,27 @@
    isSymbol(value): Return true if value is a Symbol primitive, else false.
    isUndefined(value): Return true if value is undefined, else false.
  */
+
+   export function isBoolean(value) {
+  return value === true || value === false;
+}
+
+export function isNumber(value) {
+  return typeof value === 'number';
+}
+
+export function isNull(value) {
+  return value === null;
+}
+
+export function isString(value) {
+  return typeof value === 'string';
+}
+
+export function isSymbol(value) {
+  return typeof value === 'symbol';
+}
+
+export function isUndefined(value) {
+  return value === undefined;
+}
