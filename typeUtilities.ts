b7cc20a350a2ad5c4e0adbe1154c035820e1f08f
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-22 20:29:36 +0530

    added type utility description

---
+++ typeUtilities.ts
@@ -0,0 +1,6 @@
+/**
+ * JavaScript is a dynamically typed language, which means the types of variable can be changed during runtime. 
+ * Many interview questions involve recursion of values that contain values of different types and 
+ * how to handle each value type (e.g. different code is needed to iterate over an array vs an object). 
+ * Knowledge of handling the JavaScript types is crucial to solving questions like Deep Clone and Deep Equal.
+ */
