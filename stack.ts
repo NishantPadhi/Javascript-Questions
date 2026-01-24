6e8044f6ab81b4a2d1148184bda9a589a163f87a
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-24 19:04:31 +0530

    added example

--- stack.ts
+++ stack.ts
@@ -8,3 +8,17 @@ isEmpty(): Determines if the stack is empty. Required time complexity: O(1).
 peek(): Returns the item at the top of the stack without removing it from the stack. Required time complexity: O(1).
 length(): Returns the number of items in the stack. Required time complexity: O(1).
  */
+// Example below
+/**
+ * 
+const stack = new Stack();
+stack.isEmpty(); // true
+stack.push(1);
+stack.push(2);
+stack.length(); // 2
+stack.push(3);
+stack.peek(); // 3
+stack.pop(); // 3
+stack.isEmpty(); // false
+
+ */
