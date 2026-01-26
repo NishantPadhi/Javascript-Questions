8685514d4d350f631dd7cd1e07d6ccf992afb622
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-26 19:30:09 +0530

    added solution

--- groupBy.ts
+++ groupBy.ts
@@ -27,3 +27,23 @@ groupBy([], (o) => o); // => {}
 
 groupBy([{ n: 1 }, { n: 2 }], (o) => o.m); // => { undefined: [{ n: 1 }, { n: 2 }] }
 */
+
+/**
+ * @param {Array|Object} array The array to iterate over.
+ * @param {Function} iteratee The iteratee to transform keys.
+ * @returns {Object} Returns the composed aggregate object.
+ */
+export default function groupBy(array, iteratee) {
+  const result = {};
+
+  for (const element of array) {
+    const key = iteratee(element);
+    if (!Object.prototype.hasOwnProperty.call(result, key)) {
+      result[key] = [];
+    }
+
+    result[key].push(element);
+  }
+
+  return result;
+}
