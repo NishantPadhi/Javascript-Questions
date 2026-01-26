04fc168cb51e641b020ffc0237368d9342169f64
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-26 19:29:53 +0530

    added examples

--- groupBy.ts
+++ groupBy.ts
@@ -9,5 +9,21 @@ iteratee (Function): The iteratee to transform elements. The function is invoked
 Returns
 (Object): Returns the composed aggregate object.
 
-
  */
+
+// Examples
+/**
+ *
+groupBy([6.1, 4.2, 6.3], Math.floor);
+// => { '4': [4.2], '6': [6.1, 6.3] }
+
+groupBy([{ n: 3 }, { n: 5 }, { n: 3 }], (o) => o.n);
+// => { '3': [{ n: 3 }, { n: 3 }], '5': [{ n: 5 }] }
+
+The function should return {} when array is empty and treat null / undefined keys after going through iteratee as it is.
+
+
+groupBy([], (o) => o); // => {}
+
+groupBy([{ n: 1 }, { n: 2 }], (o) => o.m); // => { undefined: [{ n: 1 }, { n: 2 }] }
+*/
