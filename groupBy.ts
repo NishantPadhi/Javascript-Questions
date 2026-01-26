bbbce9e429e3da5f3b9a32fb5500e2e6d5b40cbc
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-26 19:29:10 +0530

    added question description

---
+++ groupBy.ts
@@ -0,0 +1,13 @@
+/**
+ *
+Implement a function groupBy(array, iteratee) that takes an array and an iteratee function, and groups the values in the array based on the iteratee.
+
+groupBy(array, iteratee);
+Arguments
+array (Array): The array to iterate over.
+iteratee (Function): The iteratee to transform elements. The function is invoked with one argument: (value).
+Returns
+(Object): Returns the composed aggregate object.
+
+
+ */
