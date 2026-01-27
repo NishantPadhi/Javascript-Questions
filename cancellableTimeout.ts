780e16be0675c9ef427aec56797dce169706ce37
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-27 18:34:29 +0530

    added question description

---
+++ cancellableTimeout.ts
@@ -0,0 +1,3 @@
+/**
+ * Implement a function setCancellableTimeout, that acts like setTimeout but instead of returning a timer ID, it returns a function that when called, cancels the pending callback function. The setCancellableTimeout function should have the exact same signature as setTimeout:
+ */
