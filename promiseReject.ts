3a5a167f5f8d819a06801c324f00041c2b329f53
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-28 19:38:36 +0530

    added question description

---
+++ promiseReject.ts
@@ -0,0 +1,7 @@
+/**
+ * Unlike Promise.resolve(), Promise.reject() always wraps reason in a new Promise object, 
+ * even when reason is already a Promise.
+
+   Implement the Promise.reject() function as promiseReject. 
+   You can ignore the case where this is referenced within the implemented function.
+ */
