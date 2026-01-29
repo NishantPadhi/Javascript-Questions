e32cc5d10a138d51cbc0abf8ae2769e2526a920e
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-29 20:01:24 +0530

    added description

---
+++ formPairs.ts
@@ -0,0 +1,11 @@
+/**
+ *
+Implement a function fromPairs(pairs) that transforms a list of key-value pairs into an object.
+
+fromPairs(pairs);
+Arguments
+pairs (Array): An array of key-value pairs.
+Returns
+(Object): Returns the object composed from the key-value pairs.
+
+*/
