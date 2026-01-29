a5556af64319621cd8154f1e07d904c0a43daa65
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-29 20:02:06 +0530

    added solution

--- formPairs.ts
+++ formPairs.ts
@@ -21,3 +21,18 @@ const pairs = [
 fromPairs(pairs); // => { a: 1, b: 2, c: 3 }
 
 */
+/**
+ * Creates an object from an array of key-value pairs.
+ *
+ * @param {Array} pairs - An array of key-value pairs.
+ * @returns {Object} - The object composed from the key-value pairs.
+ */
+export default function fromPairs(pairs) {
+  const result = {};
+
+  for (const [key, value] of pairs) {
+    result[key] = value;
+  }
+
+  return result;
+}
