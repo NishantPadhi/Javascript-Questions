f7abc7f441322228502ce2b96dcd5a7e2eca2974
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-31 22:39:25 +0530

    added examples

--- cycle.ts
+++ cycle.ts
@@ -2,3 +2,13 @@
  * Implement a function that takes one or more values and 
  * returns a function that cycles through those values each time it is called.
  */
+
+// Examples
+// const helloFn = cycle('hello');
+// console.log(helloFn()); // "hello"
+// console.log(helloFn()); // "hello"
+
+// const onOffFn = cycle('on', 'off');
+// console.log(onOffFn()); // "on"
+// console.log(onOffFn()); // "off"
+// console.log(onOffFn()); // "on"
