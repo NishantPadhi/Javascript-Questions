98a999e01c859b2ca870d4af6646e9f1e1dc3960
NishantPadhi <nishantpadhi2015@gmail.com>
2026-01-31 22:39:52 +0530

    added solution

--- cycle.ts
+++ cycle.ts
@@ -12,3 +12,19 @@
 // console.log(onOffFn()); // "on"
 // console.log(onOffFn()); // "off"
 // console.log(onOffFn()); // "on"
+
+/**
+ * @template T
+ * @param  {...T} values
+ *
+ * @returns () => T
+ */
+export default function cycle(...values) {
+  let index = 0;
+
+  return () => {
+    const currentValue = values[index];
+    index = (index + 1) % values.length;
+    return currentValue;
+  };
+}
