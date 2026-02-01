88cd0ca76bcb5fcbe90eaf2e467eac9d2ec89f06
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-01 20:57:37 +0530

    added description

---
+++ intersection.ts
@@ -0,0 +1,6 @@
+/**
+ * Implement a JavaScript function intersection(arrays) that takes multiple 
+ * arrays as input and returns a new array containing the unique values that 
+ * are present in all given arrays SameValueZero for equality comparisons. 
+ * The order of values in the returned array are determined by the first array.
+*/
