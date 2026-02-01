a230e70a577f141dc6699d911df5233e5c27b06b
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-01 20:58:18 +0530

    added example

--- intersection.ts
+++ intersection.ts
@@ -4,3 +4,15 @@
  * are present in all given arrays SameValueZero for equality comparisons. 
  * The order of values in the returned array are determined by the first array.
 */
+
+/** Examples
+ *
+const arr1 = [1, 2, 3];
+const arr2 = [2, 3, 4];
+const arr3 = [3, 4, 5];
+
+intersection(arr1, arr2, arr3); // => [3]
+
+const arr4 = [1, 5, 7, 9, 7];
+intersection(arr4); // [1, 5, 7, 9]
+*/
