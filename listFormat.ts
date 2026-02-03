dfb2a2289f6f7fe362e1e22bbbfd998cfad434c5
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-03 19:20:22 +0530

    added example

--- listFormat.ts
+++ listFormat.ts
@@ -8,3 +8,40 @@ sorted: Sorts the items by alphabetical order.
 length: Show only the first length items, using "and X other(s)" for the remaining. Ignore invalid values (negative, 0, etc).
 unique: Remove duplicate items.
 */
+
+/**
+ * 
+listFormat([]); // ''
+
+listFormat(['Bob']); // 'Bob'
+listFormat(['Bob', 'Alice']); // 'Bob and Alice'
+
+listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John']);
+// 'Bob, Ben, Tim, Jane and John'
+
+listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John'], {
+  length: 3,
+}); // 'Bob, Ben, Tim and 2 others'
+
+listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John'], {
+  length: 4,
+}); // 'Bob, Ben, Tim, Jane and 1 other'
+
+listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John'], {
+  length: 3,
+  sorted: true,
+}); // 'Ben, Bob, Jane and 2 others'
+
+listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John', 'Bob'], {
+  length: 3,
+  unique: true,
+}); // 'Bob, Ben, Tim and 2 others'
+
+listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John'], {
+  length: 3,
+  unique: true,
+}); // 'Bob, Ben, Tim and 2 others'
+
+listFormat(['Bob', 'Ben', '', '', 'John']); // 'Bob, Ben and John'
+
+*/
