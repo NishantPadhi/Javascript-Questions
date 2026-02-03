eee19a65f0082d3e279e458bec6f215e75b35edd
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-03 19:20:00 +0530

    added description

---
+++ listFormat.ts
@@ -0,0 +1,10 @@
+/**
+*
+Given a list of strings, implement a function listFormat that returns the items concatenated into a single string. A common use case would be in summarizing the reactions for social media posts.
+
+The function should support a few options as the second parameter:
+
+sorted: Sorts the items by alphabetical order.
+length: Show only the first length items, using "and X other(s)" for the remaining. Ignore invalid values (negative, 0, etc).
+unique: Remove duplicate items.
+*/
