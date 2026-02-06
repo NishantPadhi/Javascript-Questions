eb7126bbfa8f6add91f72ddd3d5aae06a5662eb0
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-06 18:54:04 +0530

    added description

---
+++ makeCounter2.ts
@@ -0,0 +1,9 @@
+/**
+ *
+Implement a function makeCounter that accepts an optional integer value (defaults to 0) and returns an object that contains the following methods:
+
+get(): returns the current value.
+increment(): increments the current value and returns it.
+decrement(): decrements the current value and returns it.
+reset(): resets the current value to the initial value.
+*/
