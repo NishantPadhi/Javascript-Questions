7952c88b785db1a1ec9d4378503870946b7c876d
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-07 18:18:59 +0530

    added solution for maxBy

--- maxBy.ts
+++ maxBy.ts
@@ -19,3 +19,23 @@ The function should ignore elements where iteratee produces null or undefined.
 
 maxBy([{ n: 1 }, { n: 2 }], (o) => o.m); // => undefined
 */
+
+/**
+ * @param {Array} array The array to iterate over.
+ * @param {Function} iteratee The iteratee invoked per element.
+ * @returns {*} Returns the maximum value.
+ */
+export default function maxBy(array, iteratee) {
+  let result, computed;
+
+  // Iterate through array to find the maximum `result`.
+  for (const value of array) {
+    const current = iteratee(value);
+    // Check whether `computed` is assigned any value yet then compare with `current`, else assign an initial value to `computed` where `current` is not `null`.
+    if (current != null && (computed === undefined || current > computed)) {
+      computed = current; // Store the calculated value of the current `result`.
+      result = value;
+    }
+  }
+  return result;
+}
