553b2dba6acd4326fcd67850c558b72dfe97b4f7
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-07 18:18:14 +0530

    added desrciption

---
+++ maxBy.ts
@@ -0,0 +1,10 @@
+/**
+ *
+Implement a function maxBy(array, iteratee) that finds the element inside array with the maximum value after going through iteratee. The iteratee is invoked with one argument: (value).
+
+Arguments
+array (Array): The array to iterate over.
+iteratee (Function): The iteratee invoked per element.
+Returns
+(*): Returns the maximum value.
+*/
