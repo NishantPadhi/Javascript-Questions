091b707a4a2dbb0186316159180a74b9693c8a65
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-09 18:09:09 +0530

    added description

---
+++ sleep.ts
@@ -0,0 +1,11 @@
+/**
+ *
+In JavaScript, the setTimeout function can be used to execute callbacks after a delay. However, it introduces a layer of nesting in the code which may not be desired. It'd be nice to use setTimeout in this fashion:
+
+
+// Note: the following code contains invalid syntax and doesn't work.
+console.log('Hello!');
+setTimeout(1000);
+console.log('Bye.');
+
+*/
