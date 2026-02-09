9347cb20a5dfb92365f81600011db2f7a783eac4
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-09 18:09:53 +0530

    Added example

--- sleep.ts
+++ sleep.ts
@@ -9,3 +9,17 @@ setTimeout(1000);
 console.log('Bye.');
 
 */
+
+/**
+ * Example
+async function greeting() {
+  console.log('Hello!');
+  await sleep(2000);
+  console.log('Bye.'); // Only logs after 2000 milliseconds (2 seconds)
+}
+
+greeting();
+// t = 0: Hello!
+// t = 2000: Bye.
+
+*/
