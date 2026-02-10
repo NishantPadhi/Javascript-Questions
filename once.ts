c4f6a7d6f2adf67d27093d2fb2206a7776df31f0
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-10 18:38:46 +0530

    added example

--- once.ts
+++ once.ts
@@ -3,3 +3,21 @@
  * Subsequent calls of the function will return the result of the first invocation of the callback function. 
  * The callback function is invoked with the this binding and arguments of the created function.
  */
+
+/**
+ * Examples
+ *
+let i = 1;
+
+function incrementBy(value) {
+  i += value;
+  return i;
+}
+
+const incrementByOnce = once(incrementBy);
+incrementByOnce(2); // i is now 3; The function returns 3.
+incrementByOnce(3); // i is still 3; The function returns the result of the first invocation, which is 3.
+i = 4;
+incrementByOnce(2); // i is still 4 as it is not modified. However, the function returns the result of the first invocation, which is 3.
+
+*/
