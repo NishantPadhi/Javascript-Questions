352c3549274cbd761e98c1aa71e048533b62964d
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-10 18:38:24 +0530

    added description

---
+++ once.ts
@@ -0,0 +1,5 @@
+/**
+ * Implement a function that accepts a callback and restricts its invocation to at most once. 
+ * Subsequent calls of the function will return the result of the first invocation of the callback function. 
+ * The callback function is invoked with the this binding and arguments of the created function.
+ */
