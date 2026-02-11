451e3b0c235fc6ce6599dc111d9fc548a69af3ff
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-11 18:24:48 +0530

    added solution for flatten

--- flatten.ts
+++ flatten.ts
@@ -19,3 +19,21 @@ flatten([
 flatten([1, [2, [3, [4, [5]]]]]); // [1, 2, 3, 4, 5]
 
 */
+
+type ArrayValue = any | Array<ArrayValue>;
+
+export default function flatten(value: Array<ArrayValue>): Array<any> {
+  const res = [];
+  const copy = value.slice();
+
+  while (copy.length) {
+    const item = copy.shift();
+    if (Array.isArray(item)) {
+      copy.unshift(...item);
+    } else {
+      res.push(item);
+    }
+  }
+
+  return res;
+}
