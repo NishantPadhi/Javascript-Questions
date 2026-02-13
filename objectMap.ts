514228211c2eac2b3aa0d46bb3c0039be68118e4
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-13 18:54:35 +0530

    added question description

---
+++ objectMap.ts
@@ -0,0 +1,5 @@
+/**
+ * Implement a function objectMap(obj, fn) to return a new object containing the results of
+ *  calling a provided function on every value in the object. The function fn is called with a single argument, 
+ * the value that is being mapped/transformed.
+ */
