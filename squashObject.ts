e3608dd3fd45d98d2069419875148367662a96a4
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-16 18:49:47 +0530

    added description

---
+++ squashObject.ts
@@ -0,0 +1,4 @@
+/**
+ * Implement a function that returns a new object after squashing the input object into a single level 
+ * of depth where nested keys are "squashed" together with a period delimiter (.).
+ */
