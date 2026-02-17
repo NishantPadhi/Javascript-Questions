5eababf922f6c5702b0e27cbaf682a11b530dcca
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-17 19:28:51 +0530

    added solution

--- deepOmit.ts
+++ deepOmit.ts
@@ -21,3 +21,32 @@ const obj = {
 deepOmit(obj, ['b', 'c', 'e']); // { a: 1, f: [5, 6] }
 
 */
+/**
+ * @param {any} val
+ * @param {Array<string>} keys
+ * @returns any
+ */
+export default function deepOmit(val, keys) {
+  if (!Array.isArray(val) && !isPlainObject(val)) {
+    return val;
+  }
+
+  // Both arrays and objects can be traversed using `for...in` statements.
+  const newObj = Array.isArray(val) ? [] : {};
+  for (const key in val) {
+    if (!keys.includes(key)) {
+      newObj[key] = deepOmit(val[key], keys);
+    }
+  }
+
+  return newObj;
+}
+
+function isPlainObject(value) {
+  if (value == null) {
+    return false;
+  }
+
+  const prototype = Object.getPrototypeOf(value);
+  return prototype === null || prototype === Object.prototype;
+}
