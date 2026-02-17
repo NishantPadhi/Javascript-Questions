1384e3c69c6c4284f4e4dbb938593b50e03d200d
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-17 19:27:57 +0530

    added description

---
+++ deepOmit.ts
@@ -0,0 +1,6 @@
+/**
+ * Implement a function deepOmit(obj, keys) that removes specified keys and their corresponding values from an object, 
+ * including nested objects or arrays. It works recursively to traverse through the entire object structure, 
+ * ensuring that all occurrences of the specified keys are removed at all levels. The function takes in an object (obj) 
+ * and an array of string keys (keys).
+*/
