d29c38e6b9f31637851f618023546648471eecd9
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-18 15:43:28 +0530

    added question description

---
+++ typeUtilities3.ts
@@ -0,0 +1,13 @@
+/**
+ *
+In Type Utilities, we have implemented utility functions to determine the types of primitive values. 
+In this question, we will implement the following utility functions to determine the types of non-primitive values.
+
+isArray(value): Return true if value is an array, false otherwise.
+isFunction(value): Return true if value is a function, false otherwise.
+isObject(value): Return true if value is an object (e.g. arrays, functions, objects, etc, 
+but not including null and undefined), false otherwise.
+isPlainObject(value): Return true if value is a plain object, false otherwise (for arrays, functions, etc).
+A plain object, or what is commonly known as a Plain Old JavaScript Object (POJO) is any object whose prototype 
+is Object.prototype or an object created via Object.create(null).
+*/
