b36dd69444830fefda343c349397eee2b8f7e707
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-19 20:25:37 +0530

    added description

---
+++ minBy.ts
@@ -0,0 +1,10 @@
+/**
+ *
+Implement a function minBy(array, iteratee) that finds the element inside array with the minimum value after going through iteratee.
+
+Arguments
+array (Array): The array to iterate over.
+iteratee (Function): The iteratee invoked per element, which is a function that accepts one argument: (value).
+Returns
+(*): Returns the minimum value.
+*/
