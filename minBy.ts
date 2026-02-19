e169cbc31690fa5e0a8eb44a413beb7148c0025e
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-19 20:26:36 +0530

    added solution

--- minBy.ts
+++ minBy.ts
@@ -8,3 +8,24 @@ iteratee (Function): The iteratee invoked per element, which is a function that 
 Returns
 (*): Returns the minimum value.
 */
+
+/**
+ * @param {Array} array The array to iterate over.
+ * @param {Function} iteratee The iteratee invoked per element.
+ * @returns {*} Returns the minimum value.
+ */
+export default function minBy(array, iteratee) {
+  let result, computed;
+
+  // Iterate through array to find the minimum `result`.
+  for (const value of array) {
+    const current = iteratee(value);
+    // Check whether `computed` is assigned any value yet then compare with `current`, else assign an initial value to `computed` where `current` is not `null`.
+    if (current != null && (computed === undefined || current < computed)) {
+      computed = current; // Store the calculated value of the current `result`.
+      result = value;
+    }
+  }
+
+  return result;
+}
