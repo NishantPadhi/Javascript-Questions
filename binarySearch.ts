d25ab255aaf08c6487c179f9f81ac3114722fdfb
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-21 19:01:33 +0530

    added description

---
+++ binarySearch.ts
@@ -0,0 +1,5 @@
+/**
+ * Implement a function that performs binary search on an array of numbers. 
+ * The function should take in a sorted array of integers and a target integer to find. 
+ * It returns the index of the target element or -1, if the target element doesn't exist in the array.
+*/
