9733a9cfab1c713a121d9374c69eee7c1e771860
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-22 19:46:14 +0530

    added description

---
+++ insertionSort.ts
@@ -0,0 +1,5 @@
+/**
+ * Implement a function that performs an insertion sort. 
+ * The function should take in an array of integers and return an array with the integers sorted in ascending order. 
+ * The input array is modified.
+ */
