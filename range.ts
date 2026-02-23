be692a56b43b5586d67232c3c145fda64677b534
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-23 18:46:40 +0530

    added solution for range

---
+++ range.ts
@@ -0,0 +1,11 @@
+/**
+ *
+Implement a function range([start=0], end, [step=1]) that creates an array of numbers (positive and/or negative) progressing from start up to, but not including, end. A step of -1 is used if a negative start is specified without an end or step. If end is not specified, it's set to start with start then set to 0.
+
+Arguments
+start (Number): The start of the range.
+end (Number): The end of the range.
+step (Number): The value to increment or decrement by.
+Returns
+(Array): Returns the range of numbers.
+*/
