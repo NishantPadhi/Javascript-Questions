8f1359c8f795785799d9735989106dedfc0f8191
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-24 18:23:04 +0530

    added function description

---
+++ unionBy.ts
@@ -0,0 +1,5 @@
+/**
+ * Implement a function unionBy(array) that creates an array of unique values, in order, 
+ * from all given arrays and accepts iteratee which is invoked for each element of each arrays to generate the 
+ * criterion by which uniqueness is computed.
+ */
