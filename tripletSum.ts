62404a59283b8e9d2b0fbb1728d4704872ae4f05
NishantPadhi <nishantpadhi2015@gmail.com>
2026-02-25 18:10:36 +0530

    added question description

---
+++ tripletSum.ts
@@ -0,0 +1,5 @@
+/**
+ * Given an integer array numbers, return all triplets [numbers[i], numbers[j], numbers[k]] 
+ * such that i, j, and k are distinct indices and their values sum to 0. Ensure that the solution 
+ * set contains no duplicate triplets.
+ */
